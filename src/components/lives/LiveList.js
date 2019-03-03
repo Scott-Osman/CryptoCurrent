60713697a74390fab841ae84978c07a679eecdd2
Scott Osman <SOJob@protonmail.ch>
2019-03-03 14:39:59 +0000

    Not Getting Back Live Data, Misunderstanding of Objects, keys, values, entries, still need to add EON too

--- src/components/lives/LiveList.js
+++ src/components/lives/LiveList.js
@@ -3,8 +3,8 @@ import Live from './Live.js'
 
 const LiveList = (props) => {
 
-  const lives = props.lives.map((live) => {
-    return(<li key={live} className='component-item'><Live live = {live}/></li>)
+  const lives = props.lives.map((live, index) => {
+    return(<li key={index} className='component-item'><Live live = {live}/></li>)
   })
 
   return(
